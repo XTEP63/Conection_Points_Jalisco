0bb466fcf2eb1fb18b909216f5c94b7a77909b0a
XTEP63 <110838865+XTEP63@users.noreply.github.com>
2024-03-08 19:54:24 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A proyect for inference of the bandwidth of a public institution in Jalisco',
+    author='Esteban Javier Berumen Nieto',
+    license='MIT',
+)
